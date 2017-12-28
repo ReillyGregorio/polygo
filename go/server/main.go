da1714420b033e3417a819faeed0fc980a419bab
Reilly <Reilly Gregorio>
2017-12-28 15:14:59 -0500

    wip

--- go/server/main.go
+++ go/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"html/template"
 	"net/http"
@@ -52,6 +53,191 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type period struct {
+	/*{period:"1st",class:"Math",classroom:"Room 511"},
+	{period:"2nd",class:"CSP",classroom:"Room 2703"},
+	{period:"3rd",class:"English",classroom:"Room 2305"},
+	{period:"4th",class:"Civics",classroom:"Room 1711"},
+	{period:"5th",class:"Homeroom",classroom:"Room 503"}*/
+	Period    string `json:"period"`
+	Class     string `json:"class"`
+	Classroom string `json:"classroom"`
+}
+
+func classesHandler(w http.ResponseWriter, r *http.Request) {
+	data := []period{
+		period{
+			Period:    "1st",
+			Class:     "Math",
+			Classroom: "Room 511",
+		},
+		period{
+			Period:    "2nd",
+			Class:     "CSP",
+			Classroom: "Room 2703",
+		},
+		period{
+			Period:    "3rd",
+			Class:     "English",
+			Classroom: "Room 2305",
+		},
+		period{
+			Period:    "4th",
+			Class:     "Civics",
+			Classroom: "Room 1711",
+		},
+		period{
+			Period:    "5th",
+			Class:     "Homeroom",
+			Classroom: "Room 503",
+		},
+	}
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		glog.Errorf("Failed to encode: %s", err)
+	}
+}
+
+type messages struct {
+	/*
+			{name:"Reilly:",msg:"What was the homework yesterday"},
+		  	{name:"Isiah:",msg:"pages 5-17 I think"},
+		  	{name:"Reilly:",msg:"oh right thx"},
+		  	{name:"Generic Freshman:",msg:"I like cheese"},
+	*/
+	Name string `json:"name"`
+	Msg  string `json:"msg"`
+}
+
+func chatHandler(w http.ResponseWriter, r *http.Request) {
+	data := []messages{
+		messages{
+			Name: "Reilly:",
+			Msg:  "What was the homework yesterday",
+		},
+		messages{
+			Name: "Isiah:",
+			Msg:  "pages 5-17 I think",
+		},
+		messages{
+			Name: "Reilly:",
+			Msg:  "oh right thx",
+		},
+		messages{
+			Name: "Generic Freshman::",
+			Msg:  "WI like cheese",
+		},
+	}
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		glog.Errorf("Failed to encode: %s", err)
+	}
+}
+
+type events struct {
+	/*
+			{dow:"S",date:"Dec 24th 2017", hw:"Sleep"},
+		  	{dow:"M",date:"Dec 25th 2017", hw:"Chapter 8 page 327-390"},
+		  	{dow:"T",date:"Dec 26th 2017", hw:"Questions 1-17"},
+		  	{dow:"W",date:"Dec 27th 2017", hw:"HW Packet Page 1"},
+		  	{dow:"T",date:"Dec 28th 2017", hw:"HW Packet Page 2"},
+		  	{dow:"F",date:"Dec 29th 2017", hw:"HW Packet Page 3"},
+		  	{dow:"S",date:"Dec 30th 2017", hw:"Sleep"},
+		  	{dow:"S",date:"Dec 31st 2017", hw:"Sleep"},
+		  	{dow:"M",date:"Jan 1st 2018", hw:"HW Packet Page 4"},
+		  	{dow:"T",date:"Jan 2nd 2018", hw:"HW Packet Page 5-6"},
+		  	{dow:"W",date:"Jan 3rd 2018", hw:"Pages 5-17"},
+		  	{dow:"T",date:"Jan 4th 2018", hw:"Pages 18-27"},
+		  	{dow:"F",date:"Jan 5th 2018", hw:"Pages 28-36"},
+		  	{dow:"S",date:"Jan 6th 2018", hw:"Sleep"},
+		  	{dow:"S",date:"Jan 7th 2018", hw:"Sleep"},
+	*/
+	Dow  string `json:"dow"`
+	Date string `json:"date"`
+	Hw   string `json:"hw"`
+}
+
+func calendarHandler(w http.ResponseWriter, r *http.Request) {
+	data := []events{
+		events{
+			Dow:  "S",
+			Date: "Dec 24th 2017",
+			Hw:   "Sleep",
+		},
+		events{
+			Dow:  "M",
+			Date: "Dec 25th 2017",
+			Hw:   "Chapter 8 page 327-390",
+		},
+		events{
+			Dow:  "T",
+			Date: "Dec 26th 2017",
+			Hw:   "Questions 1-17",
+		},
+		events{
+			Dow:  "W",
+			Date: "Dec 27th 2017",
+			Hw:   "HW Packet Page 1",
+		},
+		events{
+			Dow:  "T",
+			Date: "Dec 28th 2017",
+			Hw:   "HW Packet Page 2",
+		},
+		events{
+			Dow:  "F",
+			Date: "Dec 29th 2017",
+			Hw:   "HW Packet Page 3",
+		},
+		events{
+			Dow:  "S",
+			Date: "Dec 30th 2017",
+			Hw:   "Sleep",
+		},
+		events{
+			Dow:  "S",
+			Date: "Dec 31st 2017",
+			Hw:   "Sleep",
+		},
+		events{
+			Dow:  "M",
+			Date: "Jan 1st 2018",
+			Hw:   "HW Packet Page 4",
+		},
+		events{
+			Dow:  "T",
+			Date: "Jan 2nd 2018",
+			Hw:   "HW Packet Page 5-6",
+		},
+		events{
+			Dow:  "W",
+			Date: "Jan 3rd 2018",
+			Hw:   "Pages 5-17",
+		},
+		events{
+			Dow:  "T",
+			Date: "Jan 4th 2018",
+			Hw:   "Pages 18-27",
+		},
+		events{
+			Dow:  "F",
+			Date: "Jan 5th 2018",
+			Hw:   "Pages 28-36",
+		},
+		events{
+			Dow:  "S",
+			Date: "Jan 6th 2018",
+			Hw:   "Sleep",
+		},
+		events{
+			Dow:  "S",
+			Date: "Jan 7th 2018",
+			Hw:   "Sleep",
+		},
+	}
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		glog.Errorf("Failed to encode: %s", err)
+	}
+}
+
 func main() {
 	flag.Parse()
 
@@ -61,6 +247,9 @@ func main() {
 
 	// Add page handlers here.
 	router.HandleFunc("/", indexHandler)
+	router.HandleFunc("/classes", classesHandler)
+	router.HandleFunc("/chat", chatHandler)
+	router.HandleFunc("/calendar", calendarHandler)
 
 	http.Handle("/", router)
 	glog.Infof("Server is running at: http://localhost%s", *port)
